afc9f4c5584f127a5545506252e07a08ca1301cb
Jonas Bak Phillipson <joph@atp.dk>
2022-04-28 09:49:31 +0200

    Added initial server

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    getQuote: () => "Hello world",
+  },
+};
+
+export { resolvers };
---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http";
+
+import { typeDefs } from "./graphql/typeDefs.js";
+import { resolvers } from "./graphql/resolvers/index.js";
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+(async () => {
+  await server.start();
+  server.applyMiddleware({ app, path: "/" });
+
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+})();
